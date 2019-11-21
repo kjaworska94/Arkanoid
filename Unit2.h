03b06eabb8cf7b7a10f7879cc21e2adb9ec421ae
Kasia <kjaworska@gmail.com>
2019-11-21 16:44:30 +0100

    Arkanoid-Game

---
+++ Unit2.h
@@ -0,0 +1,22 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit2H
+#define Unit2H
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+//---------------------------------------------------------------------------
+class TDataModule2 : public TDataModule
+{
+__published:	// IDE-managed Components
+private:	// User declarations
+public:		// User declarations
+        __fastcall TDataModule2(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TDataModule2 *DataModule2;
+//---------------------------------------------------------------------------
+#endif
+ 
